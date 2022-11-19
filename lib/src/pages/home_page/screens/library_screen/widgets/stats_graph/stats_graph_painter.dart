dc534af57b05ead9a53d8f6b69e26057deead7cc
Relorer <Y.tm@bk.ru>
2022-11-19 21:52:31 +0300

    update

--- lib/src/pages/home_page/screens/library_screen/widgets/stats_graph/stats_graph_painter.dart
+++ lib/src/pages/home_page/screens/library_screen/widgets/stats_graph/stats_graph_painter.dart
@@ -95,7 +95,7 @@ class StatsGraphPainter extends CustomPainter {
               pressedPosition!.dx >= x1 - offsetBetween / 2)) {
         continue;
       }
-      print(placeholder[i]);
+
       _drawColumn(canvas, x1, defaultMargin * (columnSelected ? 2 : 1), x2,
           graphHeight - defaultMargin, graphData.days[i], placeholder[i]);
 
