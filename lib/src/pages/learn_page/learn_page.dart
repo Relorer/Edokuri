23b72948638059490d9305b28e3b7b989e6f89f2
Relorer <Y.tm@bk.ru>
2022-11-28 17:52:08 +0300

    update

--- lib/src/pages/learn_page/learn_page.dart
+++ lib/src/pages/learn_page/learn_page.dart
@@ -3,10 +3,12 @@ import 'dart:math';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:freader/src/core/widgets/record_with_info_card/reader_page_sliding_up_panel.dart';
 import 'package:freader/src/models/models.dart';
 import 'package:freader/src/theme/system_bars.dart';
 import 'package:freader/src/theme/theme.dart';
 import 'package:freader/src/theme/theme_consts.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 
 class LearnPage extends StatefulWidget {
@@ -22,6 +24,7 @@ class LearnPage extends StatefulWidget {
 
 class LearnPageState extends State<LearnPage> with WidgetsBindingObserver {
   late final SwipableStackController _controller;
+  final PanelController _panelController = PanelController();
 
   void _listenController() => setState(() {});
 
@@ -60,168 +63,177 @@ class LearnPageState extends State<LearnPage> with WidgetsBindingObserver {
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
-            onPressed: () {},
+            onPressed: () => _panelController.open(),
           ),
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: SafeArea(
-        child: Column(
-          children: [
-            LinearProgressIndicator(
-              value: 0.1,
-              backgroundColor:
-                  Theme.of(context).paleElementColor.withOpacity(0.3),
-            ),
-            Expanded(
-              child: SwipableStack(
-                swipeAnchor: SwipeAnchor.top,
-                dragStartCurve: Curves.linear,
-                rewindAnimationCurve: Curves.fastOutSlowIn,
-                cancelAnimationCurve: Curves.fastOutSlowIn,
-                detectableSwipeDirections: const {
-                  SwipeDirection.right,
-                  SwipeDirection.left,
-                },
-                controller: _controller,
-                stackClipBehaviour: Clip.hardEdge,
-                onSwipeCompleted: (index, direction) {
-                  if (kDebugMode) {
-                    print('$index, $direction');
-                  }
-                },
-                horizontalSwipeThreshold: 0.7,
-                verticalSwipeThreshold: 0.7,
-                overlayBuilder: (context, properties) {
-                  final opacity = min(properties.swipeProgress, 1.0);
-                  return Padding(
-                    padding: const EdgeInsets.all(doubleDefaultMargin),
-                    child: Opacity(
-                      opacity: opacity,
-                      child: properties.direction == SwipeDirection.right
-                          ? Container(
-                              child: const Center(
-                                  child: Text(
-                                "Know",
-                                style: TextStyle(
-                                    fontSize: 32,
-                                    color: const Color(0xff14B220),
-                                    fontWeight: FontWeight.bold),
-                              )),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(defaultRadius)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      blurRadius: defaultMargin,
-                                      color: Theme.of(context)
-                                          .paleElementColor
-                                          .withOpacity(0.4),
-                                      spreadRadius: 0.0,
-                                      offset: const Offset(0, 5),
-                                    ),
-                                  ]),
-                            )
-                          : Container(
-                              child: const Center(
-                                  child: Text(
-                                "Still learning",
-                                style: TextStyle(
-                                    fontSize: 32,
-                                    color: savedWord,
-                                    fontWeight: FontWeight.bold),
-                              )),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(defaultRadius)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      blurRadius: defaultMargin,
-                                      color: Theme.of(context)
-                                          .paleElementColor
-                                          .withOpacity(0.4),
-                                      spreadRadius: 0.0,
-                                      offset: const Offset(0, 5),
-                                    ),
-                                  ]),
-                            ),
-                    ),
-                  );
-                },
-                builder: (context, properties) {
-                  final itemIndex = properties.index % widget.records.length;
-
-                  return Padding(
-                    padding: const EdgeInsets.all(doubleDefaultMargin),
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(defaultRadius)),
-                                boxShadow: [
-                                  BoxShadow(
-                                    blurRadius: defaultMargin,
-                                    color: Theme.of(context)
-                                        .paleElementColor
-                                        .withOpacity(0.4),
-                                    spreadRadius: 0.0,
-                                    offset: const Offset(0, 5),
-                                  ),
-                                ]),
-                            child: Material(
-                              color: Colors.transparent,
-                              child: Column(children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(
-                                        doubleDefaultMargin),
-                                    child: Center(
-                                      child: AutoSizeText(
-                                        widget.records[itemIndex].original,
-                                        style: TextStyle(
-                                            fontSize: 28,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: IconButton(
-                                        icon: const Icon(Icons.keyboard),
-                                        onPressed: () {
-                                          print("test");
-                                        },
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: IconButton(
-                                        icon: const Icon(Icons.remove_red_eye),
-                                        onPressed: () {},
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: doubleDefaultMargin,
-                                )
-                              ]),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
+      body: ReaderPageSlidingUpPanel(
+        blockBody: false,
+        body: SafeArea(
+          child: Column(
+            children: [
+              LinearProgressIndicator(
+                value: 0.1,
+                backgroundColor:
+                    Theme.of(context).paleElementColor.withOpacity(0.3),
               ),
-            ),
-          ],
+              Expanded(
+                child: SwipableStack(
+                  swipeAnchor: SwipeAnchor.top,
+                  dragStartCurve: Curves.linear,
+                  rewindAnimationCurve: Curves.fastOutSlowIn,
+                  cancelAnimationCurve: Curves.fastOutSlowIn,
+                  detectableSwipeDirections: const {
+                    SwipeDirection.right,
+                    SwipeDirection.left,
+                  },
+                  controller: _controller,
+                  stackClipBehaviour: Clip.hardEdge,
+                  onSwipeCompleted: (index, direction) {
+                    if (kDebugMode) {
+                      print('$index, $direction');
+                    }
+                  },
+                  horizontalSwipeThreshold: 0.7,
+                  verticalSwipeThreshold: 0.7,
+                  overlayBuilder: (context, properties) {
+                    final opacity = min(properties.swipeProgress, 1.0);
+                    return Padding(
+                      padding: const EdgeInsets.all(doubleDefaultMargin),
+                      child: Opacity(
+                        opacity: opacity,
+                        child: properties.direction == SwipeDirection.right
+                            ? Container(
+                                child: const Center(
+                                    child: Text(
+                                  "Know",
+                                  style: TextStyle(
+                                      fontSize: 32,
+                                      color: const Color(0xff14B220),
+                                      fontWeight: FontWeight.bold),
+                                )),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(defaultRadius)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        blurRadius: defaultMargin,
+                                        color: Theme.of(context)
+                                            .paleElementColor
+                                            .withOpacity(0.4),
+                                        spreadRadius: 0.0,
+                                        offset: const Offset(0, 5),
+                                      ),
+                                    ]),
+                              )
+                            : Container(
+                                child: const Center(
+                                    child: Text(
+                                  "Still learning",
+                                  style: TextStyle(
+                                      fontSize: 32,
+                                      color: savedWord,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(defaultRadius)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        blurRadius: defaultMargin,
+                                        color: Theme.of(context)
+                                            .paleElementColor
+                                            .withOpacity(0.4),
+                                        spreadRadius: 0.0,
+                                        offset: const Offset(0, 5),
+                                      ),
+                                    ]),
+                              ),
+                      ),
+                    );
+                  },
+                  builder: (context, properties) {
+                    final itemIndex = properties.index % widget.records.length;
+
+                    return Padding(
+                      padding: const EdgeInsets.all(doubleDefaultMargin),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(defaultRadius)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      blurRadius: defaultMargin,
+                                      color: Theme.of(context)
+                                          .paleElementColor
+                                          .withOpacity(0.4),
+                                      spreadRadius: 0.0,
+                                      offset: const Offset(0, 5),
+                                    ),
+                                  ]),
+                              child: Material(
+                                color: Colors.transparent,
+                                child: Column(children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(
+                                          doubleDefaultMargin),
+                                      child: Center(
+                                        child: AutoSizeText(
+                                          widget.records[itemIndex].original,
+                                          style: TextStyle(
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: IconButton(
+                                          icon: const Icon(Icons.keyboard),
+                                          onPressed: () {
+                                            print("test");
+                                          },
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: IconButton(
+                                          icon:
+                                              const Icon(Icons.remove_red_eye),
+                                          onPressed: () {},
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: doubleDefaultMargin,
+                                  )
+                                ]),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        controller: _panelController,
+        panelBuilder: (ScrollController sc) => Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: Container(color: Colors.amber),
         ),
       ),
     );
