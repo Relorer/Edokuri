e285179d82762d66372a949cc88e5e8e99ea0e00
Relorer <Y.tm@bk.ru>
2022-12-02 20:09:20 +0300

    update

--- lib/src/pages/set_page/widgets/records_screen_records_cards.dart
+++ lib/src/pages/set_page/widgets/records_screen_records_cards.dart
@@ -2,10 +2,12 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:freader/src/controllers/common/tts_controller/tts_controller.dart';
 import 'package:freader/src/controllers/stores/set_controller/set_controller.dart';
 import 'package:freader/src/models/record.dart';
 import 'package:freader/src/theme/theme.dart';
 import 'package:freader/src/theme/theme_consts.dart';
+import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class RecordsScreenRecordsCards extends StatelessWidget {
@@ -55,26 +57,31 @@ class RecordsScreenRecordsCards extends StatelessWidget {
               speed: 350,
               fill: Fill.fillBack,
               direction: FlipDirection.VERTICAL,
-              front: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.background,
-                  borderRadius: BorderRadius.circular(defaultRadius / 2),
+              front: GestureDetector(
+                onLongPress: () {
+                  context.read<TTSController>().speak(records[index].original);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                    borderRadius: BorderRadius.circular(defaultRadius / 2),
+                  ),
+                  child: Padding(
+                      padding: const EdgeInsets.all(doubleDefaultMargin),
+                      child: Stack(
+                        children: [
+                          Center(
+                              child: AutoSizeText(
+                            records[index].original.toUpperCase(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge!
+                                .copyWith(color: Colors.black87),
+                            maxLines: 8,
+                          )),
+                        ],
+                      )),
                 ),
-                child: Padding(
-                    padding: const EdgeInsets.all(doubleDefaultMargin),
-                    child: Stack(
-                      children: [
-                        Center(
-                            child: AutoSizeText(
-                          records[index].original.toUpperCase(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge!
-                              .copyWith(color: Colors.black87),
-                          maxLines: 8,
-                        )),
-                      ],
-                    )),
               ),
               back: Container(
                 decoration: BoxDecoration(
