640086477f49124e046f14d1b19efa388ad5f393
John Doe <johndoe@example.com>
2023-02-27 07:52:10 +0300

    Added back arrow as on the settings page

--- lib/src/pages/learn_page/learn_page.dart
+++ lib/src/pages/learn_page/learn_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:freader/src/core/widgets/bouncing_custom_scroll_view.dart';
 import 'package:freader/src/models/models.dart';
+
 
 class LearnPage extends StatefulWidget {
   final List<Record> records;
-
   const LearnPage({Key? key, required this.records}) : super(key: key);
 
   @override
@@ -12,11 +13,17 @@ class LearnPage extends StatefulWidget {
   }
 }
 
-class LearnPageState extends State<LearnPage> with WidgetsBindingObserver {
+class LearnPageState extends State<LearnPage> {
 
   @override
   Widget build(BuildContext context) {
 
-    return const Text("Learn");
+    return const BouncingCustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+          ),
+        ],
+      );
   }
 }
