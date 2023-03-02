99b009c4bfe8c4fd91261a2c237331809bd86798
TheFRR <52603467+TheFRR@users.noreply.github.com>
2023-03-02 21:18:30 +0300

    Empty book rejection (#25)
    * Empty book rejection
    
    Added empty book rejection and toast showing with info after rejection. Added toast service.
    
    * Added ToastController

--- lib/src/controllers/common/file_controller/file_controller.dart
+++ lib/src/controllers/common/file_controller/file_controller.dart
@@ -1,11 +1,12 @@
+import 'package:freader/src/controllers/common/toast_controller/toast_controller.dart';
 import 'package:freader/src/controllers/stores/repositories/book_repository/book_repository.dart';
-
 import 'services/epub_service.dart';
 import 'services/file_picker_service.dart';
 
 class FileController {
   final EpubService _epubService = EpubService();
   final FilePickerService _filePickerService = FilePickerService();
+  final ToastController _toastController = ToastController();
 
   final BookRepository bookRepository;
 
@@ -15,6 +16,9 @@ class FileController {
     var files = await _filePickerService.getFiles(allowedExtensions: ["epub"]);
     for (var file in files) {
       var book = await _epubService.readBook(file);
+      if (book.words.isEmpty) {
+        return _toastController.showDefaultTost("The book can't be empty");
+      }
       bookRepository.putBook(book);
     }
   }
---
+++ lib/src/controllers/common/toast_controller/toast_controller.dart
@@ -0,0 +1,11 @@
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastController {
+  void showDefaultTost(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        fontSize: 15.0);
+  }
+}
