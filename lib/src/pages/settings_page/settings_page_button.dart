5952ba7cf626f01dbd9aa0a8128ca32be481066c
relorer <y.tm@bk.ru>
2023-03-14 23:13:05 +0300

    feat: add styles for ml button

---
+++ lib/src/pages/settings_page/settings_page_button.dart
@@ -0,0 +1,32 @@
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+// 🌎 Project imports:
+import 'package:edokuri/src/core/widgets/button_with_icon.dart';
+import 'package:edokuri/src/theme/theme.dart';
+
+class SettingPageButton extends StatelessWidget {
+  final String text;
+  final String svg;
+  final GestureTapCallback? onTap;
+
+  const SettingPageButton({
+    Key? key,
+    required this.svg,
+    this.onTap,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonWithIcon(
+      color: Theme.of(context).colorScheme.background,
+      highlightColor: Theme.of(context).secondBackgroundColor,
+      style: Theme.of(context).dialogTextStyleBright.copyWith(
+          color: Theme.of(context).secondBackgroundColor.withOpacity(0.9)),
+      text: text,
+      svg: svg,
+      onTap: onTap,
+    );
+  }
+}
