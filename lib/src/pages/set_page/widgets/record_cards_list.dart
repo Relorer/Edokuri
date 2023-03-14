2f7bd55a9ef4ff51269e04897a8923ffd8183fe6
relorer <y.tm@bk.ru>
2023-03-14 21:41:31 +0300

    feat: add search records

--- lib/src/pages/set_page/widgets/record_cards_list.dart
+++ lib/src/pages/set_page/widgets/record_cards_list.dart
@@ -3,11 +3,13 @@ import 'package:flutter/widgets.dart';
 
 // 📦 Package imports:
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 
 // 🌎 Project imports:
-import 'package:edokuri/src/controllers/stores/set_controller/set_controller.dart';
+import 'package:edokuri/src/controllers/stores/search_record_controller/search_record_controller.dart';
 import 'package:edokuri/src/controllers/stores/sort_controllers/records_sort_controller/record_sort_controller.dart';
 import 'package:edokuri/src/core/service_locator.dart';
+import 'package:edokuri/src/models/models.dart';
 import 'package:edokuri/src/pages/set_page/widgets/record_card/record_card.dart';
 import 'package:edokuri/src/theme/theme_consts.dart';
 
@@ -21,17 +23,19 @@ class RecordCardsList extends StatelessWidget {
     return Observer(builder: (_) {
       final sortedRecords =
           getIt<RecordsSortController>().sort(setData.records);
+      final foundRecords =
+          context.read<SearchRecordController>().getSearchResult(sortedRecords);
       return SliverList(
         delegate: SliverChildBuilderDelegate(
             (context, index) => Padding(
                   padding: const EdgeInsets.fromLTRB(
                       defaultMargin, 0, defaultMargin, defaultMargin),
                   child: RecordCard(
-                    sortedRecords[index],
+                    foundRecords[index],
                     setData: setData,
                   ),
                 ),
-            childCount: sortedRecords.length),
+            childCount: foundRecords.length),
       );
     });
   }
