2fef9f3d54d0bd463710d4ed649c3709614e719e
Relorer <y-tm@bk.ru>
2023-03-16 21:21:24 +0300

    feat: add styles for ml button (#36)
    Co-authored-by: relorer <y.tm@bk.ru>

--- lib/src/theme/svgs.dart
+++ lib/src/theme/svgs.dart
@@ -18,6 +18,7 @@ const bookSvg = "assets/icons/book.svg";
 const exitSvg = "assets/icons/exit.svg";
 const githubMarkWhiteSvg = "assets/icons/github-mark-white.svg";
 const googleLogoSvg = "assets/icons/google_logo.svg";
+const translateSvg = "assets/icons/translate.svg";
 
 const translatedByGoogleSvg = "assets/images/translated_by_google.svg";
 const waveSvg = "assets/images/wave.svg";
