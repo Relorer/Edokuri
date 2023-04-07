cdd9d2ac79132f3fb51b4949d09b65a1ef3a97be
TheFRR <kirilllyudin@gmail.com>
2023-04-07 21:34:17 +0300

    Using temp files + try-catch

--- lib/src/controllers/common/dictionary_controller/dictionary_contoller.dart
+++ lib/src/controllers/common/dictionary_controller/dictionary_contoller.dart
@@ -1,5 +1,6 @@
 // 🐦 Flutter imports:
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 // 📦 Package imports:
@@ -13,50 +14,58 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 class DictionaryController {
-  Future<String> createTxtFile(String text) async {
-    final Directory directory = await getApplicationDocumentsDirectory();
+  Future<String> createTempTxtFile(String text) async {
+    final Directory directory = await getTemporaryDirectory();
     final File file = File('${directory.path}/dictionary.txt');
     await file.writeAsString(text);
     return file.path;
   }
 
   void shareDictionary() async {
-    String dictionary = "";
-    final records = getIt<RecordRepository>().records;
-    for (var record in records) {
-      dictionary += "${record.original}\t${record.translation}\n";
+    try {
+      String dictionary = "";
+      final records = getIt<RecordRepository>().records;
+      for (var record in records) {
+        dictionary += "${record.original}\t${record.translation}\n";
+      }
+      String path = await createTempTxtFile(dictionary);
+      Share.shareXFiles([XFile(path)]);
+    } catch (e, stacktrace) {
+      log("${e.toString()}\n${stacktrace.toString()}");
     }
-    String path = await createTxtFile(dictionary);
-    Share.shareXFiles([XFile(path)]);
   }
 
   void importDictionary() async {
-    final files =
-        await FilePickerService().getFiles(allowedExtensions: ["txt"]);
-    files.first
-        .openRead()
-        .map(utf8.decode)
-        .transform(const LineSplitter())
-        .forEach((line) async {
-      String word = "";
-      int i = 0;
-      while (line[i] != '\t') {
-        word += line[i];
-        i++;
-      }
-      String translation = line.substring(i + 1);
-      List<Translation> translations = List.empty(growable: true);
-      translation.split(',').forEach((element) {
-        translations.add(
-            Translation(element.trim(), source: userSource, selected: true));
-      });
-      if (getIt<RecordRepository>().getRecord(word) == null) {
-        Record record = await getIt<TranslatorController>().translate(word);
-        for (var translation in translations) {
-          record.translations.add(translation);
+    try {
+      final files =
+          await FilePickerService().getFiles(allowedExtensions: ["txt"]);
+      files.first
+          .openRead()
+          .map(utf8.decode)
+          .transform(const LineSplitter())
+          .forEach((line) async {
+        String word = "";
+        int i = 0;
+        while (line[i] != '\t') {
+          word += line[i];
+          i++;
         }
-        getIt<RecordRepository>().putRecord(record);
-      }
-    });
+        String translation = line.substring(i + 1);
+        List<Translation> translations = List.empty(growable: true);
+        translation.split(',').forEach((element) {
+          translations.add(
+              Translation(element.trim(), source: userSource, selected: true));
+        });
+        if (getIt<RecordRepository>().getRecord(word) == null) {
+          Record record = await getIt<TranslatorController>().translate(word);
+          for (var translation in translations) {
+            record.translations.add(translation);
+          }
+          getIt<RecordRepository>().putRecord(record);
+        }
+      });
+    } catch (e, stacktrace) {
+      log("${e.toString()}\n${stacktrace.toString()}");
+    }
   }
 }
