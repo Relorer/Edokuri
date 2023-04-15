1f4b79f4b8dc4929ea9496c10f1257dfe5431976
Relorer <y.tm@bk.ru>
2023-04-16 00:30:26 +0300

    hot fix

--- lib/src/core/utils/datetime_extensions.dart
+++ lib/src/core/utils/datetime_extensions.dart
@@ -1,5 +1,9 @@
 extension DateOnlyCompare on DateTime {
   bool isSameDate(DateTime other) {
-    return year == other.year && month == other.month && day == other.day;
+    final tempThis = toUtc();
+    final tempOther = other.toUtc();
+    return tempThis.year == tempOther.year &&
+        tempThis.month == tempOther.month &&
+        tempThis.day == tempOther.day;
   }
 }
